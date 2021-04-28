89338edf042cf413748543a836c291b33f3542f4
Tiên Nguyễn Thị <tiennt@minori.com.vn>
2021-04-28 15:15:49 +0700

    update 28/04/21

--- src/components/user/header/Header.js
+++ src/components/user/header/Header.js
@@ -115,16 +115,14 @@ class Header extends Component {
       // </div>
       <div>
         <nav className="mb-1 navbar navbar-expand-lg navbar-dark default-color">
-        <Link to="/"><a className="navbar-brand" href="#">Giới thiệu</a></Link>
+        <Link to="/"><a className="navbar-brand" >Giới thiệu</a></Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333" aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent-333">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Nhà hàng & Cửa hàng
-                <span className="sr-only">(current)</span>
-              </a>
+          <li className="nav-item active">
+              <Link to="/home/restaurant"><a className="nav-link" >Nhà hàng & Cửa hàng</a></Link>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">Dịch vụ</a>
--- src/components/user/home/Home.js
+++ src/components/user/home/Home.js
@@ -13,8 +13,8 @@ class Home extends Component {
       vendor: [],
       foodRestaurant: [],
       speakerRestaurant: [],
-      cakeRestaurant:[],
-      decorRestaurant:[]
+      cakeRestaurant: [],
+      decorRestaurant: [],
 
       // service: [],
       // newVendor: [],
@@ -25,7 +25,7 @@ class Home extends Component {
     this.getSpeakerRestaurant();
     this.getCakeRestaurant();
     this.getDeCorRestaurant();
-    
+
     // this.getService();
     // this.getNewVendor();
     // this.getTopVendor();
@@ -117,25 +117,24 @@ class Home extends Component {
 
         <div>
           <img src="./img/main4.PNG" width="100%" height="auto" alt="" />
-          <div class="jumbotron text-center">
-            <h1>QUEEN PARTY</h1>
-            <p>
-              <b>Website đặt tiệc tại nhà</b>
-            </p>
-            <p>
-              Là trang web hàng đầu tại Việt Nam tích hợp nhiều cửa hàng & nhà
-              hàng cung cấp dịch vụ nấu nướng và phục vụ tiệc như sinh nhật, tân
-              gia, ngày kỷ niệm... tại gia.Khách hàng có thể đặt các món ăn và
-              dịch vụ có sẵn trong cửa hàng hoặc tự tạo kế hoạch cho mình, sau
-              đó gửi yêu cầu đến các nhà hàng, cửa hàng.
-            </p>
-          </div>
 
           <div class="container">
+            <div class="jumbotron text-center">
+              <h1>QUEEN PARTY</h1>
+              <p>
+                <b>Website đặt tiệc tại nhà</b>
+              </p>
+              <p>
+                Là trang web hàng đầu tại Việt Nam tích hợp nhiều cửa hàng & nhà
+                hàng cung cấp dịch vụ nấu nướng và phục vụ tiệc như sinh nhật,
+                tân gia, ngày kỷ niệm... tại gia.Khách hàng có thể đặt các món
+                ăn và dịch vụ có sẵn trong cửa hàng hoặc tự tạo kế hoạch cho
+                mình, sau đó gửi yêu cầu đến các nhà hàng, cửa hàng.
+              </p>
+            </div>
             <div class="row">
-              
-                {this.state.vendor.map((item) => (
-                  <div class="col-sm-4">
+              {this.state.vendor.map((item) => (
+                <div class="col-sm-4">
                   <div className="img-container">
                     <Link to={"/home/vendor/detail/" + item.id}>
                       {" "}
@@ -147,16 +146,17 @@ class Home extends Component {
                       <h2 className="name_vendor">{item.fullname}</h2>
                     )}
                   </div>
-                  </div>
-                ))}
-            </div><hr/>
+                </div>
+              ))}
+            </div>
+            <hr />
           </div>
-          
+
           <div class="container">
-            <h3 style={{textAlign:"center"}}>THỨC ĂN</h3>
-            <div class="row">             
-                {this.state.foodRestaurant.map((foodr) => (
-                  <div class="col-sm-3">
+            <h3 style={{ textAlign: "center" }}>THỨC ĂN</h3>
+            <div class="row">
+              {this.state.foodRestaurant.map((foodr) => (
+                <div class="col-sm-3">
                   <div className="foodRestaurant">
                     <Link to={"/home/vendor/detail/" + foodr.id}>
                       {" "}
@@ -164,21 +164,22 @@ class Home extends Component {
                         src={"http://127.0.0.1:8000/storage/" + foodr.avatar}
                       />
                     </Link>
-                    <p >{foodr.name}</p>          
+                    <p>{foodr.name}</p>
                     {this.state.valueSearch && (
                       <h2 className="name_vendor">{foodr.fullname}</h2>
                     )}
                   </div>
-                  </div>
-                ))}
-            </div><hr/>
+                </div>
+              ))}
+            </div>
+            <hr />
           </div>
 
           <div class="container">
-            <h3 style={{textAlign:"center"}}>LOA MÁY</h3>
-            <div class="row">             
-                {this.state.speakerRestaurant.map((speakerr) => (
-                  <div class="col-sm-3">
+            <h3 style={{ textAlign: "center" }}>LOA MÁY</h3>
+            <div class="row">
+              {this.state.speakerRestaurant.map((speakerr) => (
+                <div class="col-sm-3">
                   <div className="foodRestaurant">
                     <Link to={"/home/vendor/detail/" + speakerr.id}>
                       {" "}
@@ -186,20 +187,21 @@ class Home extends Component {
                         src={"http://127.0.0.1:8000/storage/" + speakerr.avatar}
                       />
                     </Link>
-                    <p >{speakerr.name}</p>          
+                    <p>{speakerr.name}</p>
                     {this.state.valueSearch && (
                       <h2 className="name_vendor">{speakerr.fullname}</h2>
                     )}
                   </div>
-                  </div>
-                ))}
-            </div><hr/>
+                </div>
+              ))}
+            </div>
+            <hr />
           </div>
           <div class="container">
-            <h3 style={{textAlign:"center"}}>BÁNH KEM</h3>
-            <div class="row">             
-                {this.state.cakeRestaurant.map((cake) => (
-                  <div class="col-sm-3">
+            <h3 style={{ textAlign: "center" }}>BÁNH KEM</h3>
+            <div class="row">
+              {this.state.cakeRestaurant.map((cake) => (
+                <div class="col-sm-3">
                   <div className="foodRestaurant">
                     <Link to={"/home/vendor/detail/" + cake.id}>
                       {" "}
@@ -207,20 +209,21 @@ class Home extends Component {
                         src={"http://127.0.0.1:8000/storage/" + cake.avatar}
                       />
                     </Link>
-                    <p >{cake.name}</p>          
+                    <p>{cake.name}</p>
                     {this.state.valueSearch && (
                       <h2 className="name_vendor">{cake.fullname}</h2>
                     )}
                   </div>
-                  </div>
-                ))}
-            </div><hr/>
+                </div>
+              ))}
+            </div>
+            <hr />
           </div>
           <div class="container">
-            <h3 style={{textAlign:"center"}}>TRANG TRÍ</h3>
-            <div class="row">             
-                {this.state.decorRestaurant.map((decor) => (
-                  <div class="col-sm-3">
+            <h3 style={{ textAlign: "center" }}>TRANG TRÍ</h3>
+            <div class="row">
+              {this.state.decorRestaurant.map((decor) => (
+                <div class="col-sm-3">
                   <div className="foodRestaurant">
                     <Link to={"/home/vendor/detail/" + decor.id}>
                       {" "}
@@ -228,13 +231,13 @@ class Home extends Component {
                         src={"http://127.0.0.1:8000/storage/" + decor.avatar}
                       />
                     </Link>
-                    <p >{decor.name}</p>          
+                    <p>{decor.name}</p>
                     {this.state.valueSearch && (
                       <h2 className="name_vendor">{decor.fullname}</h2>
                     )}
                   </div>
-                  </div>
-                ))}
+                </div>
+              ))}
             </div>
           </div>
         </div>
